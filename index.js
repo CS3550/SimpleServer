eafe08884aef9c83e17d7189d0687cd46374b758
UNO User <unouser@unomaha.edu>
2019-12-05 09:35:31 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname,"static")));
+
+app.listen(8080, (err)=>{
+	if(err) return console.log(err);
+	console.log("running on 8080");
+});
